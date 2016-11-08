74adf48e9ca6e0dad7411697f88584f9c7181161
samuwyat989 <samuwyat989@ed.amdsb.ca>
2016-11-08 13:33:15 -0500

    Add project files.

---
+++ GameSetup/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace GameSetup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.draw = new System.Windows.Forms.Button();
+            this.name = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // draw
+            // 
+            this.draw.Location = new System.Drawing.Point(282, 341);
+            this.draw.Name = "draw";
+            this.draw.Size = new System.Drawing.Size(75, 23);
+            this.draw.TabIndex = 0;
+            this.draw.Text = "DRAW";
+            this.draw.UseVisualStyleBackColor = true;
+            this.draw.Click += new System.EventHandler(this.draw_Click);
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(213, 26);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(100, 20);
+            this.name.TabIndex = 1;
+            this.name.Text = "Please input name";
+            this.name.Click += new System.EventHandler(this.name_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 376);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.draw);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button draw;
+        private System.Windows.Forms.TextBox name;
+    }
+}
+
---
+++ GameSetup/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameSetup
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void draw_Click(object sender, EventArgs e)
+        {
+            string name1;
+            name1 = name.Text;
+
+            Graphics draw = this.CreateGraphics();
+            Pen Bpen = new Pen(Color.Black);
+            SolidBrush Bbrush = new SolidBrush(Color.Black);
+            Font BFont = new Font("Consolas", 25);
+            Font LFont = new Font("Consolas", 11);
+
+            Point[] grave = { new Point(100, 150), new Point(105, 150), new Point(105, 145), new Point(205, 145), new Point(205, 150),
+                new Point(210, 150), new Point(210, 350), new Point(100, 350)};
+            
+            if (name.Text == "Please input name")
+            {
+                draw.Clear(Color.White);
+                draw.DrawString("Please input a \n name above", BFont, Bbrush, 53, 150);
+            }
+            else
+            {
+                draw.Clear(Color.White);
+                draw.DrawPolygon(Bpen, grave);
+                draw.DrawLine(Bpen, 50, 350, 260, 350);
+                draw.DrawString("R.I.P", BFont, Bbrush, 103, 150);
+                draw.DrawString(name1 + " died of:", LFont, Bbrush, 103, 180);
+            }
+
+            if (name.SelectionLength == 5)
+            {
+                draw.DrawString("Yes", BFont, Bbrush, 103, 150);
+            }
+            else
+            {
+                draw.DrawString("No", BFont, Bbrush, 103, 150);
+            }
+            
+
+        }
+
+        private void name_Click(object sender, EventArgs e)
+        {
+            name.Text = "";
+        }
+    }
+}
